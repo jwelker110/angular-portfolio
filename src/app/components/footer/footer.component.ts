d4bc80bb8deb85bf548e0d88798b742d9dbdf068
Jonathan Welker <jwelker110@gmail.com>
2017-01-26 05:36:30 -0600

    Add repo url and some style to footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  repoUrl: string = 'https://github.com/jwelker110/ng2-portfolio';
 
   constructor() { }
 
