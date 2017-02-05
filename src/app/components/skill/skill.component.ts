0ea682ade0501cac9ca929129cdc4eabfc8b3df1
Jonathan Welker <jwelker110@gmail.com>
2017-02-05 09:27:07 -0600

    Initial Commit

---
+++ src/app/components/skill/skill.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-skill',
+  templateUrl: './skill.component.html',
+  styleUrls: ['./skill.component.scss']
+})
+export class SkillComponent implements OnInit {
+  @Input() skill: Skill;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+interface Skill {
+  name: string;
+  level: number;
+}
