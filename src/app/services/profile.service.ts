dc948c5031ff196c1be34e26c5a5a7b1546ec68b
Jonathan Welker <jwelker110@gmail.com>
2017-02-11 17:23:12 -0600

    Initial Commit

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from "@angular/core";
+import {Http, Response} from "@angular/http";
+import 'rxjs/add/operator/catch';
+import {Observable} from "rxjs";
+import {UserProfile} from "../models/models";
+
+@Injectable()
+export class ProfileService {
+  private _userProfile: UserProfile;
+
+  constructor(private _http: Http) {
+  }
+
+  getProfile() {
+    if (this._userProfile) {
+      return Observable.create((observer) => {
+        observer.next(this._userProfile);
+      });
+    }
+    return this._http.get('/assets/api/profile.json')
+      .catch((error: Response | any) => {
+        let errMsg: string;
+
+        if (error instanceof Response) {
+          const body = error.json() || '';
+          const err = body.error || JSON.stringify(body);
+          errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+          errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+      })
+  }
+}
