437793953372f0b97c860df3d66147df30e5e57d
Jonathan Welker <jwelker110@gmail.com>
2017-02-11 17:16:00 -0600

    Update to include projects component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppRoutingModule } from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProjectComponent } from './components/project/project.component';
-import { ProjectService } from './services/project.service';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {HomeComponent} from './components/home/home.component';
+import {ProjectComponent} from './components/project/project.component';
+import {ProjectService} from './services/project.service';
+import {ProfileService} from "./services/profile.service";
+import {SharedModule} from "./modules/shared.module";
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
@@ -17,15 +20,18 @@ import { ProjectService } from './services/project.service';
     FooterComponent,
     NavbarComponent,
     HomeComponent,
-    ProjectComponent
+    ProjectComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
-  providers: [ProjectService],
+  providers: [ProjectService, ProfileService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
