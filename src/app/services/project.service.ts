71530fc21d5857f0f0953005539d5597a6a82a26
Jonathan Welker <jwelker110@gmail.com>
2017-02-11 17:18:54 -0600

    Catch errors when trying to retrieve projects

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,12 +1,27 @@
-import { Http } from '@angular/http';
-import { Injectable } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Injectable} from '@angular/core';
+import 'rxjs/add/operator/catch';
+import {Observable} from "rxjs";
 
 @Injectable()
 export class ProjectService {
 
-  constructor(private _http: Http) { }
+  constructor(private _http: Http) {}
 
   getProjects() {
-    return this._http.get('/assets/projects.json')
-  }  
+    return this._http.get('/assets/api/projects.json')
+      .catch((error: Response | any) => {
+        let errMsg: string;
+
+        if(error instanceof Response) {
+          const body = error.json() || '';
+          const err = body.error || JSON.stringify(body);
+          errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+          errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+      });
+  }
 }
