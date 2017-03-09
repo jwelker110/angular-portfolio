0da416cd5a0b56467b30af33f08b241c6bb06c0f
Jonathan Welker <jwelker110@gmail.com>
2017-03-09 04:42:16 -0600

    Using forRoot to ensure shared services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,9 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {AppRoutingModule} from './app-routing.module';
-
 import {AppComponent} from './app.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
-import {ProjectService} from './services/project.service';
-import {ProfileService} from "./services/profile.service";
 import {SharedModule} from "./modules/shared.module";
 import {AboutComponent} from "./features/about/about.component";
 
@@ -24,9 +21,10 @@ import {AboutComponent} from "./features/about/about.component";
     FormsModule,
     HttpModule,
     AppRoutingModule,
-    SharedModule
+  SharedModule,
+    SharedModule.forRoot()
   ],
-  providers: [ProjectService, ProfileService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
